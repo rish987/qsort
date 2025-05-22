fc1825c8966e69fa806f8ba561343e0473f2d7d3
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2025-05-22 17:30:32 +0200

    wip

--- Main.lean
+++ Main.lean
@@ -1,4 +1,8 @@
 import Qsort
 
-def main : IO Unit :=
-  IO.println s!"Hello!"
+def main : IO Unit := do
+  let mut l := #[]
+  for i in [0:500000000] do
+    l := l.push i
+  l := l.reverse
+  l := Monadic.qsort l (Nat.blt) 
--- Qsort.lean
+++ Qsort.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Qsort` library.
 -- Import modules here that should be built as part of the library.
-import Qsort.Basic
+import Qsort.Pure
+import Qsort.Monadic
